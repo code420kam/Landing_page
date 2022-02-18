8879edf1c4ec09296bc758debcaef0ca507db567
K@mi1 <kamil.durhan.90@gmail.com>
2022-02-18 15:56:32 +0100

    <2.Update>

--- js/app.js
+++ js/app.js
@@ -1,32 +1,78 @@
 
 
-const activeSection = document.getElementsByClassName("your-active-class")
-console.log(activeSection);
 
 
+var activeSection = document.querySelector(".your-active-class")
+const allSectors = document.querySelectorAll("section")
 
-const isInViewport = function (activeSection) 
+
+function isInViewport(el)
 {
-    
-	const distance = activeSection.getBoundingClientRect;    
+    const rect = el.getBoundingClientRect();
     return(
-        activeSection.top = (window.innerHeight)
-        
-    )
-};
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth));
+}
 
-window.addEventListener('scroll', function () 
+function Scrolling (sectionI)
 {
-	if (isInViewport(activeSection))
-     {
-		console.log('In viewport!');
-	} else
+	const section = document.getElementById('section'+sectionI);
+	const pos = section.offsetTop;
+	//event.preventDefault();
+	window.scrollTo({
+		left: 0, 
+		top: pos,
+		behavior: 'smooth'
+	});
+}
+
+// Detecting if user Scrolling the page
+let userHasScrolled = false;  
+window.addEventListener('scroll', (e) => {  
+  userHasScrolled = true;  
+})
+//Detecting Scroll direction
+window.onscroll = function(e) {
+    if (this.oldScroll > this.scrollY === true)
     {
-    console.log('Nope...');
+        console.log("up");
+        this.oldScroll = this.scrollY;
+    }
+    else
+    {
+        console.log("down");
+        this.oldScroll = this.scrollY;
+    }
   }
-}, false);
+  
 
+    window.addEventListener('scroll', function () 
+  {
+      for(var i=0; i<allSectors.length; i++)
+      {
+          if (!isInViewport(allSectors[i]))
+        {
+           true;
+           allSectors[i].classList.remove("your-active-class");
+          console.log('In viewport!');
+          
+          
+        } 
+      else
+    {
+        allSectors[i].classList.add("your-active-class");
+        //this.event.preventDefault();
+        
+        console.log('Nope...');
+               }
+    }
+  })
 
+  
+
+//document.querySelector(".navbar__menu").getElementsByClassName("your-active-class").style="backgroundColor:red";
 
 
 function NavBar ()
@@ -37,13 +83,18 @@ function NavBar ()
 		const li = document.createElement('li');
 		const tag = document.createElement('a');	
 		const sectionName = section[i].getAttribute('data-nav');
+        const sectionClass = section[i].getAttribute("class");
 		tag.setAttribute('href',"#"+sectionName);
 		tag.setAttribute('id',"linkNumber" +[i+1]);
+        tag.setAttribute("class", sectionClass);
 		tag.innerText=sectionName;
 		li.appendChild(tag);
+        console.log(tag);
         document.getElementById("navbar__list").appendChild(li);
 		document.getElementById("linkNumber" +[i+1]).addEventListener("click", function(){
 			Scrolling(i+1)
 		});
 	};
 }
+
+
